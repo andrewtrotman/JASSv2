e01a96cabd9aaba721d47e02006b8b2e8d7f219e
Andrew Trotman <andrew@trotman.com>
2016-10-14 20:37:22 +1300

    added documentation to dynamic array

--- source/dynamic_array.h
+++ source/dynamic_array.h
@@ -1,6 +1,14 @@
 /*
 	DYNAMIC_ARRAY.H
 	---------------
+	Copyright (c) 2016 Andrew Trotman
+	Released under the 2-clause BSD license (See:https://en.wikipedia.org/wiki/BSD_licenses)
+*/
+/*!
+	@file
+	@brief Thread-safe grow-only dynamic array using the thread-safe allocator.
+	@author Andrew Trotman
+	@copyright 2016 Andrew Trotman
 */
 #pragma once
 
@@ -12,64 +20,142 @@
 
 namespace JASS
 	{
+	/*
+		CLASS DYNAMIC_ARRAY
+		-------------------
+	*/
+	/*!
+		@brief Thread-safe grow-only dynamic array using the thread-safe allocator.
+		@details The array data is stored in a linked list of chunks where each chunk is larger then the previous as the array is growing.
+		@tparam TYPE The dynamic array is an array of this type.
+	*/
 	template <typename TYPE>
 	class dynamic_array
 		{
 		protected:
+			/*
+				CLASS DYNAMIC_ARRAY::NODE
+				-------------------------
+			*/
+			/*!
+				@brief The array is stored as a lined list of nodes where each node points to some number of elements.
+			*/
 			class node
 				{
 				public:
-					TYPE *data;
-					node *next;
-					size_t allocated;
-					std::atomic<size_t> used;
+					TYPE *data;									///< The array data for this node.
+					node *next;									///< Pointer to the next node in the chain.
+					size_t allocated;							///< The size of this node's data object (in elements).
+					std::atomic<size_t> used;				///< The numner of elements in data that are used (always <= allocated).
 					
 				public:
+					/*
+						DYNAMIC_ARRAY::NODE::NODE()
+						---------------------------
+					*/
+					/*!
+						@brief Constructor
+						@param pool [in] The pool allocator used to allocate the data controled by this node.
+						@param size [in] The size (in elements) of the data to be controlled by this node.
+					*/
 					node(allocator &pool, size_t size):
-						next(nullptr),
-						allocated(size),
-						used(0)
+						next(nullptr),				// this is the end of the linked list.
+						allocated(size),			// the data array is this size (in elements).
+						used(0)						// the data array is empty.
 						{
+						/*
+							Allocate the data this node controlls.
+						*/
 						data = new (pool.malloc(size * sizeof(TYPE))) TYPE[size];
 						}
 				};
-
+			/*
+				CLASS DYNAMIC_ARRAY::ITERATOR
+				-----------------------------
+			*/
+			/*!
+				@brief C++ iterator for iterating over a dynamic_array object.
+				@details See http://www.cprogramming.com/c++11/c++11-ranged-for-loop.html for details on how to write a C++11 iterator.
+			*/
 			class iterator
 				{
 				private:
-					const node *current_node;
-					TYPE *data;
+					const node *current_node;					///< The node that this iterator is currently looking at.
+					TYPE *data;										///< Pointer to the element within current_node that this object is looking at.
 					
 				public:
+					/*
+						DYNAMIC_ARRAY::ITERATOR::ITERATOR()
+						-----------------------------------
+					*/
+					/*!
+						@brief constructor
+						@param node [in] The node that this iterator should start looking at.
+						@param element [in] Which element within node this iterator should start looking at (normally 0).
+					*/
 					iterator(node *node, size_t element):
 						current_node(node),
-						data(node == nullptr ? nullptr : node->data + element)
+						data(node == nullptr ? nullptr : node->data + element)					// the "end" is represented as (NULL, NULL)
 						{
 						/*
 							Nothing
 						*/
 						}
-					
-					bool operator!= (const iterator &other) const
+					/*
+						DYNAMIC_ARRAY::ITERATOR::OPERATOR!=()
+						-------------------------------------
+					*/
+					/*!
+						@brief Compare two iterator objects for non-equality.
+						@param other [in] The iterator object to compare to.
+					*/
+					bool operator!=(const iterator &other) const
 						{
-						if (current_node != other.current_node)
+						/*
+							If the data pointers are different then the two must be different.
+						*/
+						if (data != other.data)
 							return true;
-						else if (data != other.data)
-							return true;
-						return false;
+						else
+							return false;
 						}
  
-					int operator* () const
+					/*
+						DYNAMIC_ARRAY::ITERATOR::OPERATOR*()
+						------------------------------------
+					*/
+					/*!
+						@brief Return a reference to the element pointed to by this iterator.
+					*/
+					TYPE &operator*() const
 						{
 						return *data;
 						}
  
+					/*
+						DYNAMIC_ARRAY::ITERATOR::OPERATOR++()
+						------------------------------------
+					*/
+					/*!
+						@brief Increment this iterator.
+					*/
 					const iterator &operator++()
 						{
+						/*
+							Just move on to the next element
+						*/
 						data++;
+						
+						/*
+							but if we're past the end of the current node then move on to the next node
+						*/
 						if (data >= current_node->data + current_node->used)
 							{
 							current_node = current_node->next;
+							
+							/*
+								If we're past the end of the list then we're done.
+							*/
 							if (current_node == nullptr)
 								data = nullptr;
 							else
@@ -80,6 +166,13 @@ namespace JASS
 				};
 			
 		private:
+			/*
+				DYNAMIC_ARRAY::DYNAMIC_ARRAY()
+				------------------------------
+			*/
+			/*!
+				@brief Private constructor cannot be called
+			*/
 			dynamic_array()
 				{
 				/*
@@ -88,46 +181,102 @@ namespace JASS
 				}
 
 		public:
-			allocator &pool;
-			node *head;
-			std::atomic<node *> tail;
-			double growth_factor;
+			allocator &pool;							///< The pool allocator used for all allocation by this object.
+			node *head;									///< Pointer to the head of the linked list of blocks of data.
+			std::atomic<node *> tail;				///< Pointer to the tail of the linked list of blocks of data.  It std::atomic<> so that it can grow lock-free
+			double growth_factor;					///< The next chunk in the linked list is this much larger than the previous.
 			
 		public:
+			/*
+				DYNAMIC_ARRAY::DYNAMIC_ARRAY()
+				------------------------------
+			*/
+			/*!
+				@brief Constructor.
+				@param pool [in] The pool allocator used for all allocation done by this object.
+				@param initial_size [in] The size (in elements) of the initial allocation in the linked list.
+				@param growth_factor [in] The next node in the linked list stored an element this many times larger than the previous (as an integer).
+			*/
 			dynamic_array(allocator &pool, size_t initial_size = 1, double growth_factor = 1.4) :
 				pool(pool),
 				growth_factor(growth_factor)
 				{
+				/*
+					Allocate space for the first write
+				*/
 				head = tail = new (pool.malloc(sizeof(node))) node(pool, initial_size);
 				}
 			
+			/*
+				DYNAMIC_ARRAY::BEGIN()
+				----------------------
+			*/
+			/*!
+				@brief Return an iterator pointing to the start of the array.
+				@return Iterator pointing to start of array
+			*/
 			iterator begin(void) const
 				{
 				return iterator(head, 0);
 				}
 			
+			/*
+				DYNAMIC_ARRAY::END()
+				--------------------
+			*/
+			/*!
+				@brief Return an iterator pointing to the end of the array.
+				@return Iterator pointing to end of array
+			*/
 			iterator end(void) const
 				{
 				return iterator(nullptr, 0);
 				}
 			
+			/*
+				DYNAMIC_ARRAY::PUSH_BACK()
+				--------------------------
+			*/
+			/*!
+				@brief Add an element to the end of the array.
+				@param element [in] The element to add.
+			*/
 			void push_back(const TYPE &element)
 				{
 				do
 					{
+					/*
+						Take a copy of the pointer to the end of the list
+					*/
 					node *last = tail;
 					
+					/*
+						Take a slot (using std::atomic<>++)
+					*/
 					size_t slot = last->used++;
 
+					/*
+						If that slot is within range then copy the element into the array
+					*/
 					if (slot < last->allocated)
 						{
+						/*
+							Copy and done.
+						*/
 						last->data[slot] = element;
 						break;
 						}
 					else
 						{
+						/*
+							We've walked past the end so we allocate space for a new node (and elements in that node) and add it to the list.
+						*/
 						last->used = last->allocated;
 						node *another = new (pool.malloc(sizeof(node))) node(pool, last->allocated * growth_factor);
+						/*
+							Atomicly make it the tail and if we succeed than make the previous node in the list point to this one.
+							If we fail then the pool allocator won't take the memory back so ignore and re-try
+						*/
 						if (tail.compare_exchange_strong(last, another))
 							last->next = another;
 						}
@@ -136,39 +285,76 @@ namespace JASS
 				}
 			
 			
-			
+			/*
+				DYNAMIC_ARRAY::UNITTEST_THREAD()
+				--------------------------------
+			*/
+			/*!
+				@brief Part of the unit test this method adds elements 0..high to the array.
+				@param high [in] How many integer elements to add to the array.
+			*/
 			void unittest_thread(size_t high)
 				{
 				for (size_t counter = 0; counter < high; counter++)
 					push_back(counter);
 				}
 			
+			/*
+				DYNAMIC_ARRAY::UNITTEST()
+				-------------------------
+			*/
+			/*!
+				@brief Unit test this class
+			*/
 			static void unittest(void)
 				{
-				static constexpr size_t highest_value = 100000;
-				static constexpr size_t thread_count = 50;
-				allocator_pool memory;
-				dynamic_array<size_t> array(memory, 1, 1);
-				std::thread thread_pool[thread_count];
+				static constexpr size_t highest_value = 100000;					// add integers 0..highest_value to a dynamic_array<size_t>.
+				static constexpr size_t thread_count = 50;						// use this many concurrent threads to add those elements.
+				allocator_pool memory;													// pool allocator that holds all the memory.
+				dynamic_array<size_t> array(memory, 1, 1);						// the dynamic_array.
+				std::thread thread_pool[thread_count];								// a thread pool used to ensure all the threads are done.
 				
+				/*
+					The "test" is to have thread_count threads each adding integers 0..highest_value to the array at once.  The test is
+					considered passed if each integer appears in the array thread_count times.
+				*/
+				/*
+					Creare a number of threads all compeating to add to the same array
+				*/
 				for (size_t which = 0; which < thread_count; which++)
 					thread_pool[which] = std::thread(&dynamic_array<size_t>::unittest_thread, &array, highest_value);
+					
+				/*
+					Wait until they are all done
+				*/
 				for (size_t which = 0; which < thread_count; which++)
 					thread_pool[which].join();
 					
-					
+				/*
+					We're going to walk through the array once counting the number of times each integer occurs so
+					allocate an array of the right size, and initialise it to all 0.
+				*/
 				std::array<size_t, highest_value> check;
-				for (const auto &val : array)
-					check[val] = 0;
+				for (auto &element : check)
+					element = 0;
 					
-				for (const auto &val : array)
-					check[val]++;
+				/*
+					Walk through the array taking a talley
+				*/
+				for (const auto &element : array)
+					check[element]++;
 				
-				for (const auto &val : check)
+				/*
+					Check each talley to make sure it is the right size.
+				*/
+				for (const auto &element : check)
 					{
-					JASS_assert(val == thread_count);
+					JASS_assert(element == thread_count);
 					}
-					
+				
+				/*
+					Passed!
+				*/
 				puts("dynamic_array::PASSED");
 				}
 		};
