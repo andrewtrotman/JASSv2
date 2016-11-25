923c4f21507d73130887dcf1537c56a647e87ae5
Andrew Trotman <andrew@trotman.com>
2016-11-25 15:20:03 +1300

    tool to generate the hash of a file

---
+++ tools/hash.cpp
@@ -0,0 +1,56 @@
+/*
+	HASH.CPP
+	--------
+	Copyright (c) 2016 Andrew Trotman
+	Released under the 2-clause BSD license (See:https://en.wikipedia.org/wiki/BSD_licenses)
+
+	Generate the checksum of a file.
+*/
+/*!
+	@file
+	@author Andrew Trotman
+	@copyright 2016 Andrew Trotman
+*/
+#include <stdio.h>
+
+#include "checksum.h"
+
+/*
+	USAGE()
+	-------
+*/
+/*!
+	@brief Explain how to use this tool.
+	@param filename [in] The path to this executable.
+	@return returns 0.
+*/
+uint8_t usage(const std::string &filename)
+	{
+	printf("Usage:%s <filename> <filename> ...\n", filename.c_str());
+	return 0;
+	}
+
+/*
+	MAIN()
+	------
+*/
+/*!
+	@brief Generate the checksum of a file
+	@param argc [in] The number of parameters (filenames) to checksum
+	@param argv [in] The list of filenames to checksum
+	@return 0 on success, else failuer
+*/
+int main(int argc, char *argv[])
+	{
+	if (argc == 0)
+		exit(usage(argv[0]));
+
+	puts("Fletcher 16 Checksum");
+	for (int parameter = 1; parameter < argc; parameter++)
+		{
+		auto checksum = JASS::checksum::fletcher_16_file(argv[parameter]);
+		printf("0x%04X : %s\n",  checksum, argv[parameter]);
+		}
+
+	return 0;
+	}
