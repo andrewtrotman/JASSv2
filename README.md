0d5fb4c8736a095723dce6fa8a4bb6c5e9db46ee
Vaughan Kitchen <v.kitchen@gnoms.org>
2016-11-30 13:59:11 +1300

    integrate travis ci

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/andrewtrotman/JASSv2.svg?branch=master)](https://travis-ci.org/andrewtrotman/JASSv2)
+
 # JASSv2
 
 ## Building JASSv2
---
+++ scripts/cmake.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function cmake_ubuntu {
+  wget http://www.cmake.org/files/v3.4/cmake-3.4.1.tar.gz
+  tar -xvzf cmake-3.4.1.tar.gz
+  cd cmake-3.4.1/
+  ./configure
+  make
+  sudo make install
+  sudo update-alternatives --install /usr/bin/cmake cmake /usr/local/bin/cmake 1 --force
+}
+
+if [ "$TRAVIS_OS_NAME" = linux ]; then
+  cmake_ubuntu
+fi
