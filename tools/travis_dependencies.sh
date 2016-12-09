d7affd124f8821684460da41a7d98a4c71ccd208
Vaughan Kitchen <v.kitchen@gnoms.org>
2016-12-09 13:34:51 +1300

    travis: add a valgrind build

--- tools/travis_dependencies.sh
+++ tools/travis_dependencies.sh
@@ -2,5 +2,5 @@
 if [ "$TRAVIS_OS_NAME" == linux ]; then
   sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
   sudo apt-get update -q
-  sudo apt-get install g++-5 -y
+  sudo apt-get install g++-5 valgrind -y
 fi
