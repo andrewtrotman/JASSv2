ac2bd5c276c473318971d95ae1b05f6a2cdd3b5e
Andrew Trotman <andrew@trotman.com>
2016-12-12 10:23:30 +1300

    config for external providers

---
+++ coverity_model.c
@@ -0,0 +1,12 @@
+/*
+	COVERITY_MODEL.C
+	----------------
+	Copyright (c) 2016 Andrew Trotman
+	Released under the 2-clause BSD license (See:https://en.wikipedia.org/wiki/BSD_licenses)
+*/
+/*!
+	@file
+	@brief List of exceptions for Coverity Scan
+	@author Andrew Trotman
+	@copyright 2016 Andrew Trotman
+*/
