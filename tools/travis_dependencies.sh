440d015fe74b782e8acf75ae2158fb262b6dac02
Vaughan Kitchen <v.kitchen@gnoms.org>
2017-01-23 11:56:43 +1300

    cleanup travis config

--- tools/travis_dependencies.sh
+++ tools/travis_dependencies.sh
@@ -3,4 +3,6 @@ if [ "$TRAVIS_OS_NAME" == linux ]; then
   sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
   sudo apt-get update -q
   sudo apt-get install g++-5 valgrind -y
+  sudo ln -sf /usr/bin/g++-5 /usr/bin/c++ # for coverity
+  sudo ln -sf /usr/bin/g++-5 /usr/bin/g++ # for normal travis builds
 fi
