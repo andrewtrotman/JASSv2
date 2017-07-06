375f10d232c702fac74ce5bbd20b243dcbc3fdff
AndrewTrotman <andrew@cs.otago.ac.nz>
2017-07-06 13:40:44 +1200

    sync before move to Linux

---
+++ source/query.h
@@ -0,0 +1,29 @@
+/*
+	QUERY.H
+	-------
+	Copyright (c) 2017 Andrew Trotman
+	Released under the 2-clause BSD license (See:https://en.wikipedia.org/wiki/BSD_licenses)
+*/
+/*!
+	@file
+	@brief Everything necessary to process a query
+	@author Andrew Trotman
+	@copyright 2017 Andrew Trotman
+*/
+#pragma once
+
+
+namespace JASS
+	{
+	/*
+		CLASS QUERY
+		-----------
+	*/
+	/*!
+		@brief Everything necessary to process a query is encapsulated in an object of this type
+	*/
+	class query
+		{
+
+		};
+	}
