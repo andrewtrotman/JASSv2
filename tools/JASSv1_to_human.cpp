06b43b79fa3cdd9ef0fbc2fa601770abb122b9c8
Andrew Trotman <andrew@trotman.com>
2017-08-03 15:13:27 +1200

    tool to dump a JASSv1 index in a human readable format

---
+++ tools/JASSv1_to_human.cpp
@@ -0,0 +1,38 @@
+/*
+	JASSV1_TO_HUMAN.CPP
+	-------------------
+	Copyright (c) 2016 Andrew Trotman
+	Released under the 2-clause BSD license (See:https://en.wikipedia.org/wiki/BSD_licenses)
+*/
+
+#include <stdio.h>
+
+#include "file.h"
+
+struct vocab_tripple
+	{
+	uint64_t term;
+	uint64_t offset;
+	uint64_t impacts;
+	};
+
+/*
+	MAIN()
+	------
+*/
+int main(void)
+	{
+	std::string vocab_buffer;
+	JASS::file::read_entire_file("CIvocab.bin", vocab_buffer);
+	vocab_tripple *vocab = (vocab_tripple *)vocab_buffer.c_str();
+	auto vocab_length = vocab_buffer.size() / sizeof(vocab_tripple);
+
+
+	std::string strings;
+	JASS::file::read_entire_file("CIvocab_terms.bin", strings);
+
+	for (auto term = 0; term < vocab_length; term++)
+		{
+		puts(strings.c_str() + vocab[term].term);
+		}
+	}
