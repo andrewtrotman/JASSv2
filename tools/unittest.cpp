5ce391e6ab425cba63e80b97f9170596255daf25
Andrew Trotman <andrew@trotman.com>
2017-10-16 15:28:54 +1300

    cant take address of 0th element of an empty vector

--- tools/unittest.cpp
+++ tools/unittest.cpp
@@ -75,7 +75,6 @@ int main(void)
 
 	try
 		{
-#ifdef NEVER
 		puts("compress_integer_all");
 		JASS::compress_integer_all::unittest();
 
@@ -188,7 +187,7 @@ int main(void)
 
 		puts("compress_integer_variable_byte");
 		JASS::compress_integer_variable_byte::unittest();
-#endif
+
 		puts("compress_integer_stream_vbyte");
 		JASS::compress_integer_stream_vbyte::unittest();
 
