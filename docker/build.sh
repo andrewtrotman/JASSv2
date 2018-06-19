4ba4da05902b0cf4aa7b6b4b36f99c4088f8a02e
AndrewTrotman <andrew@cs.otago.ac.nz>
2018-06-19 17:22:15 +1200

    working towards docker container for builds on travis

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,11 @@
 git clone https://github.com/andrewtrotman/JASSv2.git
 cd JASSv2
 cd build
-cmake ..
+CFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
+CXXFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
+cmake -D FORCE_CMAKE_BUILD_TYPE=Debug .. 
 make
+valgrind -q --error-exitcode=42 ./unittest
+ci_env=`bash <(curl -s https://codecov.io/env)`
+/bin/bash <(curl -s https://codecov.io/bash)
 
