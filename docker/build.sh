d0607a35a210a499c872eafdde7c4ec40cabab8e
AndrewTrotman <andrew@cs.otago.ac.nz>
2018-06-22 17:11:56 +1200

    move JASS to docker

--- docker/build.sh
+++ docker/build.sh
@@ -2,11 +2,12 @@
 git clone https://github.com/andrewtrotman/JASSv2.git
 cd JASSv2
 cd build
-CFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
-CXXFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
+export CFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
+export CXXFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
 cmake -D FORCE_CMAKE_BUILD_TYPE=Debug .. 
 make
 valgrind -q --error-exitcode=42 ./unittest
-ci_env=`bash <(curl -s https://codecov.io/env)`
+export CODECOV_TOKEN="531ba3f8-2ebe-40a2-9f40-95ea61dc0186"
+export ci_env=`bash <(curl -s https://codecov.io/env)`
 /bin/bash <(curl -s https://codecov.io/bash)
 
