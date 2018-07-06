e7aac6441c7dbbc4d8b31622beccfb23bf75f811
AndrewTrotman <andrew@cs.otago.ac.nz>
2018-07-06 15:44:29 +1200

    third try

--- docker/build.sh
+++ docker/build.sh
@@ -12,3 +12,4 @@ export CODECOV_TOKEN="531ba3f8-2ebe-40a2-9f40-95ea61dc0186"
 export ci_env=$(bash <(curl -s https://codecov.io/env))
 /bin/bash <(curl -s https://codecov.io/bash) > ers 2> ers2
 # valgrind -q --error-exitcode=42 ./unittest
+exit 0
