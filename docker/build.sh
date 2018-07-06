df67949f5be1ec952031acbe03e91a2068826701
AndrewTrotman <andrew@cs.otago.ac.nz>
2018-07-06 14:24:54 +1200

    seperate coverage and valgrind runs

--- docker/build.sh
+++ docker/build.sh
@@ -7,8 +7,9 @@ export CFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
 export CXXFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
 cmake -D FORCE_CMAKE_BUILD_TYPE=Debug .. 
 make
-valgrind -q --error-exitcode=42 ./unittest
+./unittest
 export CODECOV_TOKEN="531ba3f8-2ebe-40a2-9f40-95ea61dc0186"
 export ci_env=$(bash <(curl -s https://codecov.io/env))
 /bin/bash <(curl -s https://codecov.io/bash) > ers 2> ers2
 
+valgrind -q --error-exitcode=42 ./unittest
