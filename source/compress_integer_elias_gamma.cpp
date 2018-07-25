2ced6b0090602d2bfe5007f8847e0edf88a4df0f
AndrewTrotman <andrew@cs.otago.ac.nz>
2018-07-25 12:53:22 +1200

    back to little endian.  It appears to work on a limited input sequence.\

--- source/compress_integer_elias_gamma.cpp
+++ source/compress_integer_elias_gamma.cpp
@@ -27,7 +27,7 @@ namespace JASS
 		/*
 			encode
 		*/
-		uint32_t into = 0;									// bit position to write into (counted from the beginning of encoded).
+		uint64_t into = 0;									// bit position to write into (counted from the beginning of encoded).
 		for (const integer *value = source; value < source + source_integers; value++)
 			{
 			uint32_t n;
@@ -39,11 +39,19 @@ namespace JASS
 			into += n;
 
 			/*
+				Move the high bit of the integer to the low bit so that we can use that bit as the end of the unary
+				This is because we rely on storing the data from the low end of the integer to the hight end so that we can dip in at any byte
+				and have the correct sequence of bits.  It also means we can return the size used in bytes rather than as 32-bit integers (so we
+				can truncate the byte sequence at any point).  It also appears to reduce the complexity of decoding.
+			*/
+			uint64_t zig_zag = ((*value & ~(1 << n)) << 1) + 1;
+
+			/*
 				Append the value in binary
 			*/
-			uint32_t shift = 31 - (into % 32) - n;
-			uint32_t pattern = static_cast<uint64_t>(*value) << shift;
-			uint32_t *address = reinterpret_cast<uint32_t *>(encoded + (into / 8));
+			size_t shift = into % 8;
+			uint64_t pattern = zig_zag << shift;
+			uint64_t *address = reinterpret_cast<uint64_t *>(encoded + (into / 8));
 			*address |= pattern;
 			into += n + 1;
 			}
@@ -61,14 +69,32 @@ namespace JASS
 		uint64_t bits_used = 0;
 		do
 			{
-			const uint32_t *source = reinterpret_cast<const uint32_t *>(source_as_void) + (bits_used / 32);
-			uint32_t value = *source;
+			/*
+				read as many bits as we can from the lowest possible address
+			*/
+			const uint64_t *source = reinterpret_cast<const uint64_t *>(reinterpret_cast<const uint8_t *>(source_as_void) + (bits_used / 8));
 
-			uint32_t unary = _lzcnt_u32(value);
-			uint32_t binary = _bextr_u32(value, 31 - unary - unary, unary + 1);
+			/*
+				dismiss the bits we're already used
+			*/
+			uint64_t value = *source >> (bits_used % 8);
 
-			value <<= unary + unary + 1;
-			bits_used = unary + unary + 1;
+			/*
+				Append the next byte because 0x80000000 uses 63 bits (31 + 31 + 1), so if we have a string of 31-bit integers than they won't all fit!
+			*/
+			value |= *(reinterpret_cast<const uint8_t *>(source_as_void) + (bits_used / 8) + 1) << (64 - (bits_used % 8));
+
+			/*
+				get the
+			*/
+			uint64_t unary = _tzcnt_u64(value);
+			uint64_t zig_zag = _bextr_u64(value, unary, unary + 1);
+
+			uint32_t hight_bit = 1UL << unary;
+			uint32_t binary = (zig_zag >> 1) | hight_bit;
+
+			value >>= unary + unary + 1;
+			bits_used += unary + unary + 1;
 
 			*decoded = binary;
 			decoded++;
@@ -85,7 +111,7 @@ namespace JASS
 		compress_integer_elias_gamma codec;
 
 		std::vector<uint8_t> buffer(1024);
-		std::vector<integer> sequence  {16, 15, 14, 13, 12};
+		std::vector<integer> sequence = {0x80000000, 0x80000001, 16, 200, 201, 202};
 		std::vector<integer> into(sequence.size());
 
 		auto encoded_length = codec.encode(&buffer[0], buffer.size(), &sequence[0], sequence.size());
