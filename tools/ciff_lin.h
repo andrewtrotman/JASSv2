e227f5759975af53ff48e913e52d662d32d262f9
Andrew Trotman <andrew@trotman.com>
2019-06-24 23:53:54 +1200

    Can read and dump Jimmy's index fie format

---
+++ tools/ciff_lin.h
@@ -0,0 +1,331 @@
+/*
+	CIFF_LIN.H
+	----------
+	Copyright (c) 2019 Andrew Trotman
+	Released under the 2-clause BSD license (See:https://en.wikipedia.org/wiki/BSD_licenses)
+*/
+/*!
+	@file
+	@author Andrew Trotman
+	@copyright 2019 Andrew Trotman
+
+	@brief Reader for Jimmy Lin's shared index format.
+	@details Jimmy uses Anserini to index and then exports using Google protocol buffers.  The
+	protocol buffer format is specified by:
+
+	@code{.unparsed}
+	syntax = "proto3";
+	package io.anserini.cidxf;
+
+	message Posting {
+	  int32 docid = 1;
+	  int32 tf = 2;
+	}
+
+	message PostingsList {
+	  string term = 1;
+	  int64 df = 2;
+	  int64 cf = 3;
+	  repeated Posting posting = 4;
+	}
+	@endcode
+
+	Where each PostingsList is written using writeDelimitedTo() and so each postings list is prefixed by a length integer.
+
+	This code provides an iterator over a file of this format (once read into memory)
+
+	For details of the encoding see: https://developers.google.com/protocol-buffers/docs/encoding
+*/
+#pragma once
+
+#include <stdint.h>
+
+#include "protobuf.h"
+
+namespace JASS
+	{
+	/*
+		CLASS CIFF_LIN
+		--------------
+	*/
+	/*!
+		@brief Reader for Jimmy Lin's shared index format.
+		@details Jimmy uses Anserini to index and then exports using Google protocol buffers.  The
+		protocol buffer format is specified by:
+
+		@code{.unparsed}
+		syntax = "proto3";
+		package io.anserini.cidxf;
+
+		message Posting {
+		  int32 docid = 1;
+		  int32 tf = 2;
+		}
+
+		message PostingsList {
+		  string term = 1;
+		  int64 df = 2;
+		  int64 cf = 3;
+		  repeated Posting posting = 4;
+		}
+		@endcode
+
+		Where each PostingsList is written using writeDelimitedTo() and so each postings list is prefixed by a length integer.
+
+		This code provides an iterator over a file of this format (once read into memory)
+
+		For details of the encoding see: https://developers.google.com/protocol-buffers/docs/encoding
+	*/
+	class ciff_lin
+		{
+		public:
+			/*
+				CLASS CIFF_LIN::POSTINGS_LIST
+				-----------------------------
+			*/
+			/*!
+				@brief A postings list with a term, df, cf, and postings list of <d,tf> pairs.
+			*/
+			class postings_list
+				{
+				public:
+					/*!
+						@enum error_code
+						@brief success or failure.
+					*/
+					enum error_code
+						{
+						OK = 0,			///< Method completed successfully
+						FAIL = 1			///< Method did not completed successfully
+						} ;
+					/*
+						CLASS CIFF_LIN::POSTING
+						-----------------------
+					*/
+					/*!
+						@brief A <docid,tf> tuple.
+					*/
+					class posting
+						{
+						public:
+							uint32_t docid;					///< The Document identifier (probably d-gap encoded).
+							uint32_t term_frequency;		///< The number of time the term occurs in document with id docid.
+						} ;
+
+				public:
+					protobuf::slice term;					///< The term as a <pointer,length> tuple.
+					uint64_t document_frequency;			///< The number of documents containing the term.
+					uint64_t collection_frequency;		///< The number of times the term occurs in the collection.
+					std::vector<posting> postings;		///< The postings list, a vector of <d,tf> tuples
+
+				private:
+					/*
+						CIFF_LIN::POSTINGS_LIST::GET_NEXT_POSTINGS_PAIR()
+						-------------------------------------------------
+					*/
+					/*!
+						@brief read a <d,tf> pair from a protobuf encoded stream
+						@param into [out] The posting pair being read
+						@param stream [in, out] A reference to a pointer to the start of the value, points to the next byte on return.
+						@param stream_end [in] A pointer to the byte after the last byte in the stream.
+						@return OK on success, FAIL on error (unknown field number)
+					*/
+					static postings_list::error_code get_next_postings_pair(postings_list::posting &into, const uint8_t *&stream, const uint8_t *stream_end)
+						{
+						while (stream < stream_end)
+							{
+							protobuf::wire_type type;
+							uint8_t field = protobuf::get_type_and_field(type, stream);
+
+							if (type == protobuf::VARINT)
+								{
+								uint64_t value = protobuf::get_uint64_t(stream);
+								if (field == 1)
+									into.docid = value;
+								else if (field == 2)
+									into.term_frequency = value;
+								else
+									return FAIL;
+								}
+							else
+								return FAIL;
+							}
+						return OK;
+						}
+
+				public:
+					/*
+						CIFF_LIN::POSTINGS_LIST::GET_NEXT_POSTINGS()
+						--------------------------------------------
+					*/
+					/*!
+						@brief read a full postings list (inclding term, df, cf, etc.) from a protobuf encoded stream
+						@param into [out] The postings list being read
+						@param stream [in, out] A reference to a pointer to the start of the value, points to the next byte on return.
+						@param stream_end [in] A pointer to the byte after the last byte in the stream.
+						@return OK on success, FAIL on error (unknown field number)
+					*/
+					static postings_list::error_code get_next_postings(postings_list &into, const uint8_t *&stream, const uint8_t *stream_end)
+						{
+						while (stream < stream_end)
+							{
+							protobuf::wire_type type;
+							uint8_t field = protobuf::get_type_and_field(type, stream);
+
+							if (type == protobuf::VARINT)
+								{
+								uint64_t value = protobuf::get_uint64_t(stream);
+
+								if (field == 2)
+									into.document_frequency = value;
+								else if (field == 3)
+									into.collection_frequency = value;
+								else
+									return FAIL;
+								}
+							else if (type == protobuf::BLOB)
+								{
+								protobuf::slice term = protobuf::get_blob(stream);
+
+								if (field == 1)
+									into.term = term;
+								else if (field == 4)
+									{
+									const uint8_t *here = term.start;
+									postings_list::posting d_tf_pair;
+									if (get_next_postings_pair(d_tf_pair, here, here + term.length) == FAIL)
+										return FAIL;
+									into.postings.push_back(d_tf_pair);
+									}
+								}
+							else
+								return FAIL;
+							}
+						return OK;
+						}
+				} ;
+				
+		private:
+			/*
+				CLASS CIFF_LIN::ITERATOR
+				------------------------
+			*/
+			/*!
+				@brief iterator class for iterating over an index
+			*/
+			class iterator
+				{
+				private:
+					const ciff_lin &source;					///< The ciff_lin being iterated over
+					const uint8_t *stream;					///< Where in the source stream we are reading from
+					size_t where;						///< Are we the start or end of the data
+					postings_list postings;			///< The postings list we just made
+
+				public:
+					/*
+						CIFF_LIN::ITERATOR::ITERATOR()
+						------------------------------
+					*/
+					/*!
+						@brief Constructor
+						@param of [in] The ciff_lin object we iterate over
+						@where [in] Either 0 for start() or file_size for end()
+					*/
+					iterator(const ciff_lin &of, size_t where) :
+						source(of),
+						stream(source.source_file),
+						where(where)
+						{
+						/* Nothing */
+						}
+
+					/*
+						CIFF_LIN::ITERATOR::OPERATOR++()
+						--------------------------------
+					*/
+					/*!
+						@brief Move on to the next postings list by constructing and storing the current one
+						@return A referece to this iterator
+					*/
+					const iterator &operator++()
+						{
+						int64_t postings_list_length = protobuf::get_uint64_t(stream);
+
+						postings_list::get_next_postings(postings, stream, stream + postings_list_length);
+
+						return *this;
+						}
+
+					/*
+						CIFF_LIN::ITERATOR::OPERATOR*()
+						-------------------------------
+					*/
+					/*!
+						@brief Return the most recently constructed postings list
+						@return A reference to the current postings list
+					*/
+					const postings_list &operator*() const
+						{
+						return postings;
+						}
+
+					/*
+						CIFF_LIN::ITERATOR::OPERATOR!=()
+						--------------------------------
+					*/
+					/*!
+						@brief Compare two iterators.
+						@return True if the iterators are different
+					*/
+					bool operator!=(const iterator &with) const
+						{
+						return stream + where != with.stream + with.where;
+						}
+				};
+
+		private:
+			const uint8_t *source_file;		///< The CIFF file in memory
+			size_t source_file_length;			///< The length of the CIFF file in bytes
+
+		public:
+			/*
+				CIFF_LIN::CIFF_LIN()
+				--------------------
+			*/
+			/*!
+				@brief Constructor
+				@param source_file [in] a Pointer to the protobuf file once already read into memory.
+				@param source_file_length [in] The length (in bytes) of the source file once in memory.
+			*/
+			ciff_lin(const uint8_t *source_file, size_t source_file_length) :
+				source_file(source_file),
+				source_file_length(source_file_length)
+				{
+				/* Nothing */
+				}
+
+			/*
+				CIFF_LIN::BEGIN()
+				-----------------
+			*/
+			/*!
+				@brief Return and iterator to the start of this object
+			*/
+			const iterator begin() const
+				{
+				return ++iterator(*this, 0);
+				}
+
+			/*
+				CIFF_LIN::END()
+				---------------
+			*/
+			/*!
+				@brief Return and iterator to the end of this object
+			*/
+			const iterator end() const
+				{
+				return iterator(*this, source_file_length);
+				}
+		} ;
+	}
---
+++ tools/protobuf.h
@@ -0,0 +1,286 @@
+/*
+	PROTOBUF.H
+	----------
+	Copyright (c) 2019 Andrew Trotman
+	Released under the 2-clause BSD license (See:https://en.wikipedia.org/wiki/BSD_licenses)
+*/
+/*!
+	@file
+	@author Andrew Trotman
+	@copyright 2019 Andrew Trotman
+
+	@brief Hand crafted methods to read protocol buffer (protobuf) encoded files.
+	@details For details of the encoding see: https://developers.google.com/protocol-buffers/docs/encoding
+*/
+#pragma once
+
+#include <stdint.h>
+
+namespace JASS
+	{
+	/*
+		CLASS PROTOBUF
+		--------------
+	*/
+	/*!
+		@brief Functions to read a protobuf buffer
+		@details For details of the encoding see: https://developers.google.com/protocol-buffers/docs/encoding
+		This class is written to be standalone from JASSv2 so that others can use it without including all of JASSv2
+	*/
+	class protobuf
+		{
+		public:
+			/*
+				CLASS PROTOBUF::SLICE
+				---------------------
+			*/
+			/*!
+				@brief A <pointer, length> tupple used to point into a buffer without copying part of it.
+			*/
+			class slice
+				{
+				public:
+					const uint8_t *start;			///< Pointer to data
+					size_t length;				///< Length of the data being pointed to
+
+				public:
+					/*
+						PROTOBUF::SLICE::SLICE()
+						------------------------
+					*/
+					/*!
+						@brief Constructor
+						@param start [in] pointer to data
+						@param length [in] the length of that data
+					*/
+					slice(const uint8_t *start, size_t length) :
+						start(start),
+						length(length)
+						{
+						/* Nothing */
+						}
+
+					/*
+						PROTOBUF::SLICE::SLICE()
+						------------------------
+					*/
+					/*!
+						@brief Constructor
+					*/
+					slice() : slice(0,0)
+						{
+						/* Nothing */
+						}
+				} ;
+
+			/*
+				ENUM PROTOBUF::WIRE_TYPE
+				------------------------
+			*/
+			/*!
+				@enum wire_type
+				@brief The known protobuf types for protobuf 3.
+			*/
+			enum wire_type
+				{
+				VARINT = 0,					///< Variable byte encoded integer of up-to 64 bits, signed or unsigned
+				SIXTY_FOUR_BIT = 1,		///< Little endian 64-bit number, integer or floating point types
+				BLOB = 2,					///< Blob or string.  length VARINT then data
+				GROUP_START = 3,			///< Depricated in protobuf 3
+				GROUP_END = 4,				///< Depricated in protobuf 3
+				THIRTY_TWO_BIT = 5		///< Little endian 32-bit number, integer or floating point types
+				};
+
+		private:
+			/*
+				PROTOBUF::UN_ZIGZAG()
+				---------------------
+			*/
+			/*!
+				@brief Turn unsigned ZigZaged integer into a signed integer.
+				@details The spec (https://developers.google.com/protocol-buffers/docs/encoding) states:
+				"ZigZag encoding maps signed integers to unsigned integers so that numbers with a small absolute
+				value (for instance, -1) have a small varint encoded value too. It does this in a way that "zig-zags"
+				back and forth through the positive and negative integers, so that -1 is encoded as 1, 1 is encodedas
+				2, -2 is encoded as 3, and so on". This method undoes the encoding.
+				@param bits [in] The ZigZag encoding to convert
+				@return The signed integer
+			*/
+			inline static int64_t un_zigzag(uint64_t bits)
+				{
+				return (int64_t)((bits >> 1) ^ -(bits & 0x1));
+				}
+
+		public:
+			/*
+				PROTOBUF::GET_UINT64_T()
+				------------------------
+			*/
+			/*
+				@brief Read and decode an unsigned 64-bit VARINT integer.
+				@details The spec (https://developers.google.com/protocol-buffers/docs/encoding) states:
+				"Each byte in a varint, except the last byte, has the most significant bit (msb) set - this indicates
+				that there are further bytes to come. The lower 7 bits of each byte are used to store the two's complement
+				representation of the number in groups of 7 bits, least significant group first."
+				@param stream [in, out] A reference to a pointer to the start of the value, points to the next byte on return.
+				@return The decoded integer
+			*/
+			uint64_t static get_uint64_t(const uint8_t *&stream)
+				{
+				uint64_t got;
+				uint64_t next;
+				uint8_t shift = 0;
+
+				if (((got = *stream++) & 0x80) == 0)
+					return got;
+
+				got &= 0x7F;
+
+				while (1)
+					{
+					shift += 7;
+					if (((next = *stream++) & 0x80) == 0)
+						return got | next << shift;
+					got |= (next & 0x7F) << shift;
+					}
+
+				return 0;
+				}
+
+			/*
+				PROTOBUF::GET_INT64_T()
+				-----------------------
+			*/
+			/*
+				@brief Read and decode a signed 64-bit VARINT integer.
+				@param stream [in, out] A reference to a pointer to the start of the value, points to the next byte on return.
+				@return The decoded integer
+			*/
+			inline static uint64_t get_int64_t(const uint8_t *&stream)
+				{
+				return un_zigzag(get_uint64_t(stream));
+				}
+
+			/*
+				PROTOBUF::GET_UINT32_T()
+				------------------------
+			*/
+			/*
+				@brief Read and decode an unsigned 32-bit VARINT integer.
+				@param stream [in, out] A reference to a pointer to the start of the value, points to the next byte on return.
+				@return The decoded integer
+			*/
+			inline static uint32_t get_uint32_t(const uint8_t *&stream)
+				{
+				return static_cast<uint32_t>(un_zigzag(get_uint64_t(stream)));
+				}
+
+			/*
+				PROTOBUF::GET_INT32_T()
+				-----------------------
+			*/
+			/*
+				@brief Read and decode a signed 32-bit VARINT integer.
+				@param stream [in, out] A reference to a pointer to the start of the value, points to the next byte on return.
+				@return The decoded integer
+			*/
+			inline static int32_t get_int32_t(const uint8_t *&stream)
+				{
+				return static_cast<int32_t>(un_zigzag(get_uint64_t(stream)));
+				}
+
+			/*
+				PROTOBUF::GET_BLOB()
+				--------------------
+			*/
+			/*
+				@brief Read and decode a blob or string.
+				@details The spec (https://developers.google.com/protocol-buffers/docs/encoding) states:
+				"The value is a varint encoded length followed by the specified number of bytes of data."
+				@param stream [in,out] A reference to a pointer to the start of the value, points to the next byte on return.
+				@return A slice of stream containing the blob
+			*/
+			inline static slice get_blob(const uint8_t *&stream)
+				{
+				size_t length = get_uint64_t(stream);
+				const uint8_t *at = &stream[0];
+				stream += length;
+
+				return slice(at, length);
+				}
+
+			/*
+				PROTOBUF::GET_64_T()
+				--------------------
+			*/
+			/*
+				@brief Read and decode a 64-bit number.
+				@details The spec (https://developers.google.com/protocol-buffers/docs/encoding) states:
+				"Non-varint numeric types are simple – double and fixed64 have wire type 1, which tells the parser to expect a
+				fixed 64-bit lump of data" and "The values are stored in little-endian byte order."
+				@param stream [in, out] A reference to a pointer to the start of the value, points to the next byte on return.
+				@return The decoded integer
+			*/
+			inline static uint64_t get_64_t(const uint8_t *&stream)
+			{
+			uint64_t answer;
+
+			answer = static_cast<uint64_t>(*stream++) << 0;
+			answer = static_cast<uint64_t>(*stream++) << 8;
+			answer = static_cast<uint64_t>(*stream++) << 16;
+			answer = static_cast<uint64_t>(*stream++) << 24;
+			answer = static_cast<uint64_t>(*stream++) << 32;
+			answer = static_cast<uint64_t>(*stream++) << 40;
+			answer = static_cast<uint64_t>(*stream++) << 48;
+			answer = static_cast<uint64_t>(*stream++) << 56;
+
+			return answer;
+			}
+
+			/*
+				PROTOBUF::GET_32_T()
+				--------------------
+			*/
+			/*
+				@brief Read and decode a 64-bit number.
+				@details The spec (https://developers.google.com/protocol-buffers/docs/encoding) states:
+				"Non-varint numeric types are simple" ... "float and fixed32 have wire type 5, which tells it to expect 32 bits"
+				and "The values are stored in little-endian byte order."
+				@param stream [in, out] A reference to a pointer to the start of the value, points to the next byte on return.
+				@return The decoded integer
+			*/
+			inline static uint32_t get_32_t(const uint8_t *&stream)
+			{
+			uint32_t answer;
+
+			answer = static_cast<uint32_t>(*stream++) << 0;
+			answer = static_cast<uint32_t>(*stream++) << 8;
+			answer = static_cast<uint32_t>(*stream++) << 16;
+			answer = static_cast<uint32_t>(*stream++) << 24;
+
+			return answer;
+			}
+
+			/*
+				PROTOBUF::GET_TYPE_AND_FIELD()
+				------------------------------
+			*/
+			/*!
+				@brief Extract the field number and its type from the stream
+				@details The spec (https://developers.google.com/protocol-buffers/docs/encoding) states:
+				"Each key in the streamed message is a varint with the value (field_number << 3) | wire_type" ...
+				"in other words, the last three bits of the number store the wire type.".
+				@param type [out] The type of the field
+				@param stream [in, out] A reference to a pointer to the start of the value, points to the next byte on return.
+				@return The number of the field
+			*/
+			static uint8_t get_type_and_field(wire_type &type, const uint8_t *&stream)
+				{
+				uint8_t encoding = *stream++;
+
+				type = static_cast<wire_type>(encoding & 0x07);
+
+				return encoding >> 3;
+				}
+		} ;
+	}
