0d841c684f8428afb95c8e62ebaf045c4014d27f
AndrewTrotman <andrew@cs.otago.ac.nz>
2019-08-21 17:35:53 +1200

    travis build on mac

--- docker/build.sh
+++ docker/build.sh
@@ -3,13 +3,13 @@ set -ev
 git clone https://github.com/andrewtrotman/JASSv2.git
 cd JASSv2
 cd build
-export CFLAGS="-g -O0 -fprofile-arcs -ftest-coverage -fno-inline -fno-elide-constructors"
-export CXXFLAGS="-g -O0 -fprofile-arcs -ftest-coverage -fno-inline -fno-elide-constructors"
+#export CFLAGS="-g -O0 -fprofile-arcs -ftest-coverage -fno-inline -fno-elide-constructors"
+#export CXXFLAGS="-g -O0 -fprofile-arcs -ftest-coverage -fno-inline -fno-elide-constructors"
 cmake -D FORCE_CMAKE_BUILD_TYPE=Debug .. 
 make
 ./unittest
-export CODECOV_TOKEN="531ba3f8-2ebe-40a2-9f40-95ea61dc0186"
-export ci_env=$(bash <(curl -s https://codecov.io/env))
-/bin/bash <(curl -s https://codecov.io/bash) > ers 2> ers2
+#export CODECOV_TOKEN="531ba3f8-2ebe-40a2-9f40-95ea61dc0186"
+#export ci_env=$(bash <(curl -s https://codecov.io/env))
+#/bin/bash <(curl -s https://codecov.io/bash) > ers 2> ers2
 valgrind -q --error-exitcode=42 ./unittest
 exit 0
---
+++ docker/mac_build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ev
+
+#
+# We're already done the git clone and changed directories
+#
+#git clone https://github.com/andrewtrotman/JASSv2.git
+#cd JASSv2
+#cd build
+
+export CFLAGS="-fprofile-instr-generate -fcoverage-mapping -g -O0"
+export CXXFLAGS="-fprofile-instr-generate -fcoverage-mapping -g -O0"
+cmake -D FORCE_CMAKE_BUILD_TYPE=Debug .. 
+make
+
+LLVM_PROFILE_FILE="coverage/unittest.profraw" ./unittest
+xcrun llvm-profdata merge -sparse coverage/unittest.profraw -o coverage/unittest.profdata
+xcrun llvm-cov show ./unittest -instr-profile=unittest.profdata > coverage.txt
+
+export CODECOV_TOKEN="531ba3f8-2ebe-40a2-9f40-95ea61dc0186"
+export ci_env=$(bash <(curl -s https://codecov.io/env))
+/bin/bash <(curl -s https://codecov.io/bash) > ers 2> ers2
+
+exit 0
