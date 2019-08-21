9e99a9e56a06e0a2c820472ce3ea7f5ba9ec5bbe
AndrewTrotman <andrew@cs.otago.ac.nz>
2019-08-22 11:54:49 +1200

    codecov errors to output

--- docker/mac_build.sh
+++ docker/mac_build.sh
@@ -19,6 +19,6 @@ xcrun llvm-cov show ./unittest -instr-profile=coverage/unittest.profdata > cover
 
 export CODECOV_TOKEN="531ba3f8-2ebe-40a2-9f40-95ea61dc0186"
 export ci_env=$(bash <(curl -s https://codecov.io/env))
-/bin/bash <(curl -s https://codecov.io/bash) > ers 2> ers2
+/bin/bash <(curl -s https://codecov.io/bash) 
 
 echo "Done"
