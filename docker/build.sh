5ca9c8aef2399e3c9f58ffa60e9c26462cb3c7c6
AndrewTrotman <andrew@cs.otago.ac.nz>
2019-08-22 15:55:42 +1200

    Coveralls Docker Works Locally

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -ev
+
+pip install --upgrade setuptools
+pip install wheel
+pip install cpp-coveralls
+
 git clone https://github.com/andrewtrotman/JASSv2.git
 cd JASSv2
 cd build
-export CFLAGS="-g -O0 -fprofile-arcs -ftest-coverage -fno-inline -fno-elide-constructors"
+export CFLAGS="-g -O0 -fprofile-arcs -ftest-coverage -fno-inline"
 export CXXFLAGS="-g -O0 -fprofile-arcs -ftest-coverage -fno-inline -fno-elide-constructors"
 cmake -D FORCE_CMAKE_BUILD_TYPE=Debug .. 
 make
@@ -11,17 +16,15 @@ make
 #
 # Coverage using codecov.io
 #
-#export CODECOV_TOKEN="531ba3f8-2ebe-40a2-9f40-95ea61dc0186"
-#export ci_env=$(bash <(curl -s https://codecov.io/env))
-#/bin/bash <(curl -s https://codecov.io/bash) > ers 2> ers2
+# export CODECOV_TOKEN="531ba3f8-2ebe-40a2-9f40-95ea61dc0186"
+# export ci_env=$(bash <(curl -s https://codecov.io/env))
+# /bin/bash <(curl -s https://codecov.io/bash) > ers 2> ers2
 
 #
 # Coverage using coveralls.io
 #
 export COVERALLS_REPO_TOKEN=D5tNCS2tc1XK8guPC5ePloTuxr7ifkzlV
-pip install --user cpp-coveralls
 coveralls --gcov-options '\-lp'
-
 
 valgrind -q --error-exitcode=42 ./unittest
 exit 0
