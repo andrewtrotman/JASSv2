9c73d81459ed620be29605ee597b316f6c8fa64e
AndrewTrotman <andrew@cs.otago.ac.nz>
2019-11-08 12:19:22 +1300

    recall

---
+++ source/evaluate_recall.cpp
@@ -0,0 +1,40 @@
+/*
+	EVALUATE_RECALL.CPP
+	-------------------
+	Copyright (c) 2019 Andrew Trotman
+	Released under the 2-clause BSD license (See:https://en.wikipedia.org/wiki/BSD_licenses)
+*/
+#include <cmath>
+
+#include "asserts.h"
+#include "evaluate_map.h"
+#include "unittest_data.h"
+#include "evaluate_recall.h"
+
+namespace JASS
+	{
+	/*
+		EVALUATE_RECALL::COMPUTE()
+		--------------------------
+	*/
+	double evaluate_recall::compute(const std::string &query_id, const std::vector<std::string> &results_list, size_t depth) const
+		{
+		double relevant = relevance_count(query_id);
+		double found_and_relevant = 0;
+		size_t which = 0;
+
+		for (const auto &result : results_list)
+			{
+			auto assessment = find(query_id, result);
+
+			found_and_relevant += assessment.score;
+
+			which++;
+
+			if (which >= depth)
+				break;
+			}
+
+		return found_and_relevant / (relevant < depth ? relevant : depth);
+		}
+	}
---
+++ source/evaluate_recall.h
@@ -0,0 +1,53 @@
+/*
+	EVALUATE_RECALL.H
+	-----------------
+	Copyright (c) 2019 Andrew Trotman
+	Released under the 2-clause BSD license (See:https://en.wikipedia.org/wiki/BSD_licenses)
+*/
+/*!
+	@file
+	@brief Compute the recall of the results list
+	@author Andrew Trotman
+	@copyright 2019 Andrew Trotman
+*/
+#pragma once
+
+#include "evaluate.h"
+
+namespace JASS
+	{
+	/*
+		CLASS EVALUATE_RECALL
+		---------------------
+	*/
+	/*!
+		@brief Compute the recall given a results (where recall is found_and_relevant / relevant)
+	*/
+	class evaluate_recall : public evaluate
+		{
+		public:
+			using evaluate::evaluate;
+			/*
+				EVALUATE_RECALL::COMPUTE()
+				--------------------------
+			*/
+			/*!
+				@brief Compute the recall as found_and_relevant / relevant.
+				@copydoc evaluate::compute()
+			*/
+			virtual double compute(const std::string &query_id, const std::vector<std::string> &results_list, size_t depth = std::numeric_limits<size_t>::max()) const;
+
+			/*
+				EVALUATE_PRECISION::UNITTEST()
+				------------------------------
+			*/
+			/*!
+				@brief Unit test this class
+			*/
+			static void unittest(void)
+				{
+				unittest_one<evaluate_recall>(1.0);
+				puts("evaluate_recall::PASSED");
+				}
+		};
+	}
