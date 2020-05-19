fabfb11774a6eb618a2a2a4e3b8ae945850c7b34
Andrew Trotman <andrew@trotman.com>
2020-05-19 20:03:03 +1200

    correct length calculation

--- source/simd.h
+++ source/simd.h
@@ -233,7 +233,7 @@ namespace JASS
 				/*
 					Loop over all the data (going too far if necessary)
 				*/
-				__m256i *end = (__m256i *)(data + (length + 7) / 8);
+				__m256i *end = ((__m256i *)data) + ((length + 7) / 8);
 				for (__m256i *block = (__m256i *)data; block < end; block++)
 					{
 					/*
