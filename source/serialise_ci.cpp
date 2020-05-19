dd2a5acd00a3583dac3b479cea9c63c65681fb15
Andrew Trotman <andrew@trotman.com>
2020-05-19 19:21:42 +1200

    changed checksums

--- source/serialise_ci.cpp
+++ source/serialise_ci.cpp
@@ -149,25 +149,26 @@ namespace JASS
 		/*
 			Checksum the index to make sure its correct (debug on the left, release on the right)
 		*/
-//		std::cout << "=====\n";
+		std::cout << "=====\n";
 
 		auto checksum = checksum::fletcher_16_file("JASS_postings.cpp");
-//		std::cout << "JASS_postings.c:" << checksum << '\n';
-		JASS_assert(checksum == 19095 || checksum == 30121);
+		std::cout << "JASS_postings.c:" << checksum << '\n';
+		JASS_assert(checksum == 35708 || checksum == 5482);
 
 		checksum = checksum::fletcher_16_file("JASS_postings.h");
-//		std::cout << "JASS_postings.h:" << checksum << '\n';
-		JASS_assert(checksum == 16311 || checksum == 22729);
+		std::cout << "JASS_postings.h:" << checksum << '\n';
+		JASS_assert(checksum == 31263 || checksum == 44045);
 
 		checksum = checksum::fletcher_16_file("JASS_vocabulary.cpp");
-//		std::cout << "JASS_vocabulary.cpp:" << checksum << '\n';
-		JASS_assert(checksum == 51247 || checksum == 8513);
+		std::cout << "JASS_vocabulary.cpp:" << checksum << '\n';
+		JASS_assert(checksum == 8513 || checksum == 51247);
 
 		checksum = checksum::fletcher_16_file("JASS_primary_keys.cpp");
-//		std::cout << "JASS_primary_keys.cpp:" << checksum << '\n';
-		JASS_assert(checksum == 44075 || checksum == 54333);
+		std::cout << "JASS_primary_keys.cpp:" << checksum << '\n';
+		JASS_assert(checksum == 54333 || checksum == 44075);
 
-//		std::cout << "=====\n";
+		std::cout << "=====\n";
+
 		puts("serialise_ci::PASSED");
 		}
 	}
