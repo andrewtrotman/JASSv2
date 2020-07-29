33d27d1322429b1002b6f7970059731aa01be2c7
AndrewTrotman <andrew@cs.otago.ac.nz>
2020-07-29 16:02:39 +1200

    removed functors and back to defaults

--- source/query.h
+++ source/query.h
@@ -4,14 +4,9 @@
 	#define INCLUDE_FILE(x) QUOTEME(x)
 	#include INCLUDE_FILE(JASS_HAS_EXTERNAL_CONFIGURATION)
 #else
-//	#define QUERY_HEAP
-//	#define ACCUMULATOR_STRATEGY_2D
-//	#define JASS_TOPK_SORT
-
-#define QUERY_HEAP
-#define ACCUMULATOR_STRATEGY_2D
-#define JASS_TOPK_SORT
-
+	#define QUERY_HEAP
+	#define ACCUMULATOR_STRATEGY_2D
+	#define JASS_TOPK_SORT
 #endif
 
 /*
