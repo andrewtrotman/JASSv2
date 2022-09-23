1cffb1a44d435af0c498f440ad95cdb7ca6c0b3c
Andrew Trotman <andrew@cs.otago.ac.nz>
2022-09-23 13:39:27 +1200

    allow queries with up-to 65535 terms in them - previous limit was 4096

--- source/query_term_list.h
+++ source/query_term_list.h
@@ -33,7 +33,7 @@ namespace JASS
 		friend std::ostream &operator<<(std::ostream &stream, const query_term_list &object);
 
 		private:
-			static const size_t max_query_terms = 0xFFF;			///< We allow up-to this numnber of (nont necessarily unique) terms in a query.
+			static const size_t max_query_terms = 0xFFFF;			///< We allow up-to this numnber of (not necessarily unique) terms in a query.
 
 		private:
 			size_t terms_in_query;								///< The numner of terms in this query.
